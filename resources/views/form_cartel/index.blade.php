3bcac8bbd797c3f3c67e6f8ca54534474f86ed12
LuisVilla12 <luisji2312ia@gmail.com>
2026-03-24 10:31:25 -0600

    migrate update v7

--- resources/views/form_cartel/index.blade.php
+++ resources/views/form_cartel/index.blade.php
@@ -45,6 +45,8 @@
                             <th class="p-2 text-white">N°</th>
                             <th class="p-2 text-white">Autores </th>
                             <th class="p-2 text-white">Instituto</th>
+                            <th class="p-2 text-white">Correo</th>
+                            <th class="p-2 text-white">Tematica</th>
                             <th class="p-2 text-white">Cartel</th>
                             <th class="p-2 text-white">Resumen</th>
                             <th class="p-2">Acciones</th>
@@ -66,6 +68,12 @@
                                 </td>
                                 <td class="p-2">
                                     {{ $dato->institucion }}
+                                </td>
+                                <td class="p-2">
+                                    {{ $dato->correo }}
+                                </td>
+                                <td class="p-2">
+                                    {{ $dato->tematica }}
                                 </td>
                                 <td class="p-2">
                                     <a href="{{ asset('storage/' . $dato->url_cartel) }}"
