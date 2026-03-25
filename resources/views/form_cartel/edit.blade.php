d5c77079d4aa0ac142eef402d7f26ae2b9185b92
LuisVilla12 <luisji2312ia@gmail.com>
2026-03-24 22:23:33 -0600

    Add new campo

--- resources/views/form_cartel/edit.blade.php
+++ resources/views/form_cartel/edit.blade.php
@@ -103,18 +103,6 @@
                     <p class="text-red-600 text-xs mt-1">{{ $message }}</p>
                 @enderror
             </div>
-            {{-- Correo --}}
-            <div>
-                <label class="block text-sm font-semibold text-gray-700 mb-1">
-                    Correo institucional: *
-                </label>
-                <input type="email" name="correo" id="correo" value="{{ $dato->correo }}" required
-                    placeholder="Ingrese su correo institucional"
-                    class="w-full px-3 py-2 border border-gray-300 rounded-md text-sm focus:outline-none focus:ring-2 focus:ring-blue-900">
-                @error('correo')
-                    <p class="text-red-600 text-xs mt-1">{{ $message }}</p>
-                @enderror
-            </div>
             <div>
                 <label class="block text-sm font-semibold text-gray-700 mb-1">
                     Temática: *
